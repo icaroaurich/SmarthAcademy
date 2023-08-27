6f741b9221e45c17540918028beff5897977f814
icaroaurich <icaroaurich@hotmail.com>
2023-08-27 13:38:33 -0300

    Adicionado programação que conecta o ESP ao sensor ultrasonic e inserir no banco de dados caso presença seja menos de 1 metro

--- ESP/Credentials.h
+++ ESP/Credentials.h
@@ -4,7 +4,7 @@
 char ssid[] = "Aurich";            
 char pass[] = "12345678";        
 
-char user[]         = "usuario"; // "espressif" foi o usuário criado no início
-char password[]     = "senha";     // senha criada com o usuário
+char user[]         = "root"; // "espressif" foi o usuário criado no início
+char password[]     = "new_sql";     // senha criada com o usuário
 
 #endif    //Credentials_h
